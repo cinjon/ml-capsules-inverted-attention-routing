1b2ea488c20a37603cfb7e1b9d258ff961426ad8
cinjon <cinjon@nyu.edu>
2020-05-11 16:28:16 -0400

    new configs.

--- configs/resnet_backbone_movingmnist2_20ccgray_img40.py
+++ configs/resnet_backbone_movingmnist2_20ccgray_img40.py
@@ -7,7 +7,7 @@ config = {
             "stride": 2,
             "padding": 1,
             "out_img_size": 32,
-            "inp_img_size": 64,
+            "inp_img_size": 40,
         },
         "primary_capsules": {
             "kernel_size": 3,
@@ -16,7 +16,7 @@ config = {
             "caps_dim": 36,  # output dim is 1024 = num_caps * caps_dim
             "num_caps": 16,
             "padding": 0,
-            "out_img_size": 15
+            "out_img_size": 9,
         },
         "capsules": [{
             "type": "CONV",
@@ -25,15 +25,15 @@ config = {
             "kernel_size": 3,
             "stride": 1,
             "matrix_pose": True,
-            "out_img_size": 13,
+            "out_img_size": 7,
         }, {
             "type": "FC",
-            "num_caps": 10,
+            "num_caps": 16, # was 10
             "caps_dim": 36,
             "matrix_pose": True,
         }],
         "class_capsules": {
-            "num_caps": 20,
+            "num_caps": 16, # was 20
             "caps_dim": 36,
             "matrix_pose": True,
             "object_dim": 36,
